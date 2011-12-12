76f9237bbbde925f3a643f001069d3cc152cc2c8
ruho <ruho@smidur>
2011-12-12 22:13:16 +0100

    ...

--- scatter.py
+++ scatter.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+from matplotlib import use
+use("TkAgg")
+
 import numpy as np
 import pylab as pl
 from numpy.random import randn, rand
@@ -15,21 +18,21 @@ from ellipse import EllipsePatch
 from ellipse import ellipse_polar
 from ellipse import pol2cat
 
-
 def fit_ellipse(x, y):
 
     # fit raw data
     el  = Ellipse(x, y)
-    elg = EllipseGeometric(x, y, p0=(el.center[0], el.center[1], el.a, el.b, el.phi0))
-    # plot fit in normal form 
+    elg = EllipseGeometric(x, y, p0=(el.center[0], el.center[1],
+                                     el.a, el.b, el.phi0))
+    # plot fit in normal form
     el_hl1 = EllipsePatch( (0, 0), 2*el.a, 2*el.b, fill=False, color='g')
     # plot fit of raw data
     el_raw = EllipsePatch( tuple(el.center), 2*el.a, 2*el.b,
-                           angle=np.degrees(el.phi0), fill=False, color='r', 
+                           angle=np.degrees(el.phi0), fill=False, color='r',
                            lw=2)
 
-    el_gfit = EllipsePatch( tuple(elg.center), 2*elg.a, 2*el.b, 
-                            angle=np.degrees(el.phi0), fill=False, color='y', 
+    el_gfit = EllipsePatch( tuple(elg.center), 2*elg.a, 2*el.b,
+                            angle=np.degrees(el.phi0), fill=False, color='y',
                             lw=2)
 
 
@@ -52,23 +55,19 @@ def fit_ellipse(x, y):
     print 'a, b: %.3f, %.3f' %(elg.a, elg.b)
     print 'Alpha: %.3f' %elg.phi0
 
-    
+
 def setLimits(ax):
-    
     lmax = max(list(ax.get_xlim())+list(ax.get_ylim()))
     ax.set_ylim((-lmax, lmax))
     ax.set_xlim(ax.get_ylim())
     ax.set_title('Deviation')
 
-
 def getAxes():
-
     fig = figure()
-    ax = fig.add_subplot(111, aspect='equal') 
+    ax = fig.add_subplot(111, aspect='equal')
     ax.axhline(y=0.0, color='k')
     ax.axvline(x=0.0, color='k')
-
-    return fig, gca()
+    return fig, ax
 
 
 if __name__ == '__main__':
@@ -80,13 +79,14 @@ if __name__ == '__main__':
     r0 = ellipse_polar(phi, 4, rand(), rand()*np.pi)
     x, y = pol2cat(phi, r0+err, deg=False)
 
-#  data= np.loadtxt('dipoletest.csv', delimiter=',')
-#  x = data[1:,0] - data[0,0]
-#  y = data[1:,1] - data[0,1]
-       
-    el = fit_ellipse(x, y)    
-    
-  
+    # data= np.loadtxt('dipoletest2.csv', delimiter=',')
+    # x = data[1:, 0] - data[0, 0]
+    # y = data[1:, 1] - data[0, 1]
+#    import pdb; pdb.set_trace()
+
+    el = fit_ellipse(x, y)
+
+
 
     show()
-  
+
