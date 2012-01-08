2a2972a755a4c2db690aaf9c31eb37536472711c
ruho rudolf.hoefler@gmail.com <>
2012-01-08 20:25:11 +0100

    distuils setup added

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+setup.py
+
+Distutils setup script for ellipse_fit
+"""
+
+__author__ = 'rudolf.hoefler@gmail.com'
+__copyright__ = 'WTFL'
+__svn_id__ = '$Id$'
+
+import os, sys
+import glob
+
+from distutils.core import setup, Extension
+
+data_files =[(os.path.join(sys.prefix, 'share', 'fit_ellipse') ,
+              glob.glob('data/*'))
+             ]
+
+setup(
+    name='ellipse_fit',
+    version = 1.0,
+    description = 'Package to fit ellipses',
+    author = 'Rudolf Hoefler',
+    author_email = 'rudolf.hoefler@gmail.com',
+    maintainer = 'Rudolf Hoefler',
+    maintainer_email = 'rudolf.hoefler@gmail.com',
+    package_dir = {'ellipse_fit': 'src'},
+    data_files = data_files,
+    packages = ['ellipse_fit'],
+    )
