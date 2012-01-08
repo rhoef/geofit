a58b036b1bf8be41c68be8606633e1468155653c
ruho rudolf.hoefler@gmail.com <>
2012-01-08 20:31:41 +0100

    name fixes

--- setup.py
+++ setup.py
@@ -9,18 +9,18 @@ __author__ = 'rudolf.hoefler@gmail.com'
 __copyright__ = 'WTFL'
 __svn_id__ = '$Id$'
 
-import os, sys
+import os
+import sys
 import glob
 
 from distutils.core import setup, Extension
 
-data_files =[(os.path.join(sys.prefix, 'share', 'fit_ellipse') ,
-              glob.glob('data/*'))
-             ]
-
+data_files =[ (os.path.join(sys.prefix, 'share', 'ellipse_fit') ,
+               glob.glob('data/*'))
+              ]
 setup(
     name='ellipse_fit',
-    version = 1.0,
+    version = str(1.0),
     description = 'Package to fit ellipses',
     author = 'Rudolf Hoefler',
     author_email = 'rudolf.hoefler@gmail.com',
